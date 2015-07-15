24360bb621d8653828487f3096fd9f77a38eedb8
Antonio Fabregat <fabregat.antonio@gmail.com>
2015-07-15 18:35:58 +0100

    getPathwayWithDiagram method added

--- src/main/java/org/reactome/web/pwp/client/manager/state/StateHelper.java
+++ src/main/java/org/reactome/web/pwp/client/manager/state/StateHelper.java
@@ -2,6 +2,12 @@ package org.reactome.web.pwp.client.manager.state;
 
 import org.reactome.web.pwp.model.classes.DatabaseObject;
 import org.reactome.web.pwp.model.classes.Event;
+import org.reactome.web.pwp.model.classes.Pathway;
+import org.reactome.web.pwp.model.factory.DatabaseObjectFactory;
+import org.reactome.web.pwp.model.handlers.AncestorsCreatedHandler;
+import org.reactome.web.pwp.model.handlers.DatabaseObjectLoadedHandler;
+import org.reactome.web.pwp.model.util.Ancestors;
+import org.reactome.web.pwp.model.util.Path;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -32,5 +38,42 @@ public abstract class StateHelper {
             }
         }
         return rtn;
+    }
+
+    public interface PathwayWithDiagramHandler {
+        void setPathwayWithDiagram(Pathway pathway);
+        void onPathwayWithDiagramRetrievalError(Throwable throwable);
+    }
+
+    //TODO: Use Path in first instance before call StateHelper to get the PathwayDiagram
+    public static void getPathwayWithDiagram(Pathway pathway, Path path, final PathwayWithDiagramHandler handler){
+        DatabaseObjectFactory.getAncestors(pathway, new AncestorsCreatedHandler() {
+            @Override
+            public void onAncestorsLoaded(Ancestors ancestors) {
+                for (Path ancestor : ancestors) {
+                    Pathway diagram = ancestor.getLastPathwayWithDiagram();
+                    if (diagram != null) { //The pathway with diagram object needs to be filled before sending it back
+                        diagram.load(new DatabaseObjectLoadedHandler() {
+                            @Override
+                            public void onDatabaseObjectLoaded(DatabaseObject databaseObject) {
+                                handler.setPathwayWithDiagram((Pathway) databaseObject);
+                            }
+
+                            @Override
+                            public void onDatabaseObjectError(Throwable trThrowable) {
+                                handler.onPathwayWithDiagramRetrievalError(trThrowable);
+                            }
+                        });
+                        return;
+                    }
+                }
+                handler.setPathwayWithDiagram(null);
+            }
+
+            @Override
+            public void onAncestorsError(Throwable exception) {
+                handler.onPathwayWithDiagramRetrievalError(exception);
+            }
+        });
     }
 }
