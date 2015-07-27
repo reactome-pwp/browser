6b8b0237c6de758d267ad738849a81ed16407d5a
Antonio Fabregat <fabregat.antonio@gmail.com>
2015-07-27 11:50:33 +0100

    finer event tracking implemented

--- src/main/java/org/reactome/web/pwp/client/manager/analytics/GAManager.java
+++ src/main/java/org/reactome/web/pwp/client/manager/analytics/GAManager.java
@@ -1,14 +1,25 @@
 package org.reactome.web.pwp.client.manager.analytics;
 
 import com.google.gwt.core.client.JavaScriptException;
+import com.google.gwt.core.client.Scheduler;
 import com.google.gwt.event.shared.EventBus;
+import com.google.gwt.user.client.Command;
+import org.reactome.web.pwp.client.common.Selection;
+import org.reactome.web.pwp.client.common.events.DatabaseObjectSelectedEvent;
+import org.reactome.web.pwp.client.common.events.DetailsTabChangedEvent;
 import org.reactome.web.pwp.client.common.events.StateChangedEvent;
+import org.reactome.web.pwp.client.common.handlers.DatabaseObjectSelectedHandler;
+import org.reactome.web.pwp.client.common.handlers.DetailsTabChangedHandler;
 import org.reactome.web.pwp.client.common.handlers.StateChangedHandler;
 import org.reactome.web.pwp.client.common.module.BrowserModule;
 import org.reactome.web.pwp.client.common.utils.Console;
+import org.reactome.web.pwp.client.manager.state.State;
 import org.reactome.web.pwp.client.manager.title.event.TitleChangedEvent;
 import org.reactome.web.pwp.client.manager.title.handler.TitleChangedHandler;
+import org.reactome.web.pwp.model.classes.DatabaseObject;
 import uk.ac.ebi.pwp.utils.analytics.client.GATracker;
+
+import java.util.Objects;
 
 /**
  * GAManager keeps track of those events that are of interest in order to get statistics of the web application.
@@ -16,30 +27,25 @@ import uk.ac.ebi.pwp.utils.analytics.client.GATracker;
  * A part from the "trackPageview" option that GA offers, we track some internal events that have been defined to
  * know how the users use the application. NOTE: trackPageview does not differentiate pages by token (string after #)
  *
- * CATEGORY: The name supplied for the group of objects we want to track.
- * ACTION: A string that is uniquely paired with each category (commonly used to define the type of user interaction).
- * LABEL: An optional string to provide additional dimensions to the event data.
- *
- * There are actions like peptide selection that can be done in more than one place (PCM, PeptideTable or StateManager),
- * using Label we can differentiate the actions in order to know later on what the user use the most when working with
- * the webapp
- *
  * @author Antonio Fabregat <fabregat@ebi.ac.uk>
  */
-public class GAManager implements BrowserModule.Manager, StateChangedHandler, TitleChangedHandler {
+public class GAManager implements BrowserModule.Manager,
+        DatabaseObjectSelectedHandler, DetailsTabChangedHandler,
+        StateChangedHandler, TitleChangedHandler {
+
     private static final String PREFIX = "\t\t[GAManager] ";
 
-    private EventBus eventBus;
+    private State currentState;
 
     //Set to true in order to see in the console what the GAManager is doing
     private static final boolean TRACK_GA_MANAGER = true;
     private boolean gaTrackerActive;
 
     public GAManager(EventBus eventBus) {
-        this.eventBus = eventBus;
-
-        this.eventBus.addHandler(StateChangedEvent.TYPE, this);
-        this.eventBus.addHandler(TitleChangedEvent.TYPE, this);
+        eventBus.addHandler(DatabaseObjectSelectedEvent.TYPE, this);
+        eventBus.addHandler(DetailsTabChangedEvent.TYPE, this);
+        eventBus.addHandler(StateChangedEvent.TYPE, this);
+        eventBus.addHandler(TitleChangedEvent.TYPE, this);
 
         LocationHelper.Location location = LocationHelper.getLocation();
         try{
@@ -69,8 +75,35 @@ public class GAManager implements BrowserModule.Manager, StateChangedHandler, Ti
     }
 
     @Override
-    public void onStateChanged(StateChangedEvent event) {
+    public void onDatabaseObjectSelected(DatabaseObjectSelectedEvent event) {
+        String module = event.getSource().getClass().getSimpleName();
+        String action = "SELECTED";
 
+        Selection selection = event.getSelection();
+        if(selection!=null){
+            if(!Objects.equals(selection.getDiagram(), currentState.getPathway())){
+                trackEvent(selection.getDiagram(), action, module);
+            }
+            if(!Objects.equals(selection.getDatabaseObject(), currentState.getSelected())){
+                trackEvent(selection.getDatabaseObject(), action, module);
+            }
+        }
+    }
+
+    @Override
+    public void onDetailsTabChanged(DetailsTabChangedEvent event) {
+        String module = event.getSource().getClass().getSimpleName();
+        trackEvent(event.getDetailsTab().toString(), "SELECTED", module);
+    }
+
+    @Override
+    public void onStateChanged(final StateChangedEvent event) {
+        Scheduler.get().scheduleDeferred(new Command() {
+            @Override
+            public void execute() {
+                currentState = new State(event.getState());
+            }
+        });
     }
 
     @Override
@@ -80,6 +113,27 @@ public class GAManager implements BrowserModule.Manager, StateChangedHandler, Ti
         }
         if(TRACK_GA_MANAGER){
             Console.info(PREFIX + "Event tracked: [ Page name changed to : \"" + event.getTitle() + "\"]");
+        }
+    }
+
+    private void trackEvent(DatabaseObject element, String action, String module){
+        if(element==null) return;
+        String category = element.getSchemaClass().toString();
+        trackEvent(category, action, module);
+    }
+
+    private void trackEvent(String category, String action, String module){
+        if(category==null || action == null || module == null) return;
+
+        category = category.toUpperCase();
+        module = module.replace("Presenter", "").replace("TAB", "_TAB").toUpperCase();
+        action = action.toUpperCase();
+
+        if(gaTrackerActive){
+            GATracker.trackEvent(category, action, module);
+        }
+        if(TRACK_GA_MANAGER){
+            Console.info(PREFIX + "Event tracked: [" + category + ", " + action + ", " + module + "]");
         }
     }
 }
