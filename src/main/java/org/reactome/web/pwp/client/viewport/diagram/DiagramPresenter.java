e7d7ad5a9952457db0136dd1e268cee4fa9ef797
Antonio Fabregat <fabregat.antonio@gmail.com>
2015-09-29 19:49:16 +0100

    minor change

--- src/main/java/org/reactome/web/pwp/client/viewport/diagram/DiagramPresenter.java
+++ src/main/java/org/reactome/web/pwp/client/viewport/diagram/DiagramPresenter.java
@@ -128,11 +128,12 @@ public class DiagramPresenter extends AbstractPresenter implements Diagram.Prese
     @Override
     public void onStateChanged(StateChangedEvent event) {
         State state = event.getState();
+        boolean forceLoad = !Objects.equals(this.pathway, state.getPathway());
         this.pathway = state.getPathway();
         this.selected = state.getSelected();
         this.path = state.getPath();
         this.analysisStatus = state.getAnalysisStatus();
-        if(this.display.isVisible()){
+        if(forceLoad && this.display.isVisible()){
             this.loadCurrentPathway();
         }
     }
