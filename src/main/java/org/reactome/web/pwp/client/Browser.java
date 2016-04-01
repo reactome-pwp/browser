700cc98864d4d38e6ea26734dfe31010c3137dbf
Antonio Fabregat <fabregat.antonio@gmail.com>
2016-04-01 17:42:27 +0100

    new version (3.2)

--- src/main/java/org/reactome/web/pwp/client/Browser.java
+++ src/main/java/org/reactome/web/pwp/client/Browser.java
@@ -16,7 +16,7 @@ import org.reactome.web.pwp.client.manager.title.event.TitleChangedEvent;
  */
 public class Browser implements EntryPoint {
 
-    public static final String VERSION = "3.1";
+    public static final String VERSION = "3.2";
     public static final Boolean BETA = false;
 
     public static boolean VERBOSE = true;
