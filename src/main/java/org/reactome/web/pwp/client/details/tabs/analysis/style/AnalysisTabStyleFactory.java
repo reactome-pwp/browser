5e6d39341b92b128c176ead8e7b72d9cfca89211
Kostas Sidiropoulos <ksidiro@ebi.ac.uk>
2019-02-09 22:09:32 +0000

    Styling and graphics updated

--- src/main/java/org/reactome/web/pwp/client/details/tabs/analysis/style/AnalysisTabStyleFactory.java
+++ src/main/java/org/reactome/web/pwp/client/details/tabs/analysis/style/AnalysisTabStyleFactory.java
@@ -3,9 +3,11 @@ package org.reactome.web.pwp.client.details.tabs.analysis.style;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.resources.client.ClientBundle;
 import com.google.gwt.resources.client.CssResource;
+import com.google.gwt.resources.client.ImageResource;
 
 /**
  * @author Antonio Fabregat <fabregat@ebi.ac.uk>
+ * @author Kostas Sidiropoulos <ksidiro@ebi.ac.uk>
  */
 public abstract class AnalysisTabStyleFactory {
 
@@ -22,16 +24,45 @@ public abstract class AnalysisTabStyleFactory {
         /**
          * The styles used in this widget.
          */
-        @Source(AnalysisTabStyle.DEFAULT_CSS)
-        AnalysisTabStyle css();
+        @Source(ResourceCSS.DEFAULT_CSS)
+        ResourceCSS css();
 
+        @Source("../images/results_tab.png")
+        ImageResource resultsIcon();
+
+        @Source("../images/notfound_tab.png")
+        ImageResource notFoundIcon();
+
+        @Source("../images/downloads_tab.png")
+        ImageResource downloadsIcon();
+
+        @Source("../images/project_to_human.png")
+        ImageResource projectToHumanIcon();
+
+        @Source("../images/include_interactors.png")
+        ImageResource includeInteractorsIcon();
+
+        @Source("../images/warning.png")
+        ImageResource warningIcon();
+
+        @Source("../images/cluster.png")
+        ImageResource clusterIcon();
+
+        @Source("../images/filter.png")
+        ImageResource filterIcon();
+
+        @Source("../images/help.png")
+        ImageResource helpIcon();
+
+        @Source("../images/back.png")
+        ImageResource backIcon();
     }
 
     /**
      * Styles used by this module.
      */
     @CssResource.ImportedWithPrefix("reactome-analysisPanel")
-    public interface AnalysisTabStyle extends CssResource {
+    public interface ResourceCSS extends CssResource {
         /**
          * The path to the default CSS styles used by this resource.
          */
@@ -40,6 +71,32 @@ public abstract class AnalysisTabStyleFactory {
         String analysisTabSummary();
 
         String analysisTabSummaryInfo();
+
+        String summaryInfoPanel();
+
+        String overlay();
+
+        String overlayOnActionsPanel();
+
+        String summaryItem();
+
+        String resourcePanel();
+
+        String namePanel();
+
+        String optionBadge();
+
+        String actionsPanel();
+
+        String warningBtn();
+
+//        String clusterBtn();
+//
+//        String filterBtn();
+//
+//        String helpBtn();
+
+
 
 //        String analysisOptions();
 
@@ -52,5 +109,23 @@ public abstract class AnalysisTabStyleFactory {
 //        String notFoundIdentifiers();
 //
 //        String notFoundIdentifierSelected();
+
+        String unselectable();
+
+        String undraggable();
+
+        String tabButtonsPanel();
+
+        String tabButton();
+
+        String tabButtonSelected();
+
+        String tabBadge();
+
+        String panelHeader();
+
+        String panelFooter();
+
+        String downloadContainer();
     }
 }
